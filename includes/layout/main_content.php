2329558f9565fa2803be93f6e6888a6e25f487c2
Tien Nguyen <tien@thichlinux.org>
2013-03-21 14:33:17 +0700

    Chỉnh sửa style, tạo thêm tabzilla.

--- includes/layout/main_content.php
+++ includes/layout/main_content.php
@@ -12,7 +12,6 @@
 					và đưa vào lưu thông trên thị trường.</span>
 					<a class="more"><span>Xem chi tiết</span></a>
 				</div>
-				<div class="clearfix"></div>
 				<div class="linksWrapper">
 					<ul class="moreLinks">
 						<li class="linkItem"><a>Biểu phí kiểm tra</a></li>
@@ -43,6 +42,69 @@
 						<li class="linkItem"><a>Quy trình kiểm định</a></li>
 						<li class="linkItem"><a>Biểu phí kiểm định</a></li>
 						<li class="linkItem"><a>Gửi yêu cầu kiểm định</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+        <div class="featureContent featureOdd">
+			<h2 class="title">Huấn Luyện Giám Sát An Toàn Lao Động</h2>
+			<div class="textFeature">
+				<div class="a">
+					<div class="imageWrapper">
+						<img class="image" src="http://kiemdinh.vn/images/h3.gif" alt="" />
+					</div>
+					<span>Huấn luyện an toàn là yêu cầu bắt buộc của Bộ Luật lao động. Các khóa huấn luyện
+                    của Trung tâm được thiết kế phù hợp với thực tế, do các kỹ sư chuyên ngành có kinh nghiệm
+                    giảng dạy.</span>
+					<a class="more"><span>Xem chi tiết</span></a>
+				</div>
+				<div class="linksWrapper">
+					<ul class="moreLinks">
+						<li class="linkItem"><a>Tham khảo nội dung các khóa học</a></li>
+						<li class="linkItem"><a>Gửi yêu cầu huấn luyện</a></li>
+						<li class="linkItem"><a>Thông tư 37/2005/TT-BLĐTBXH hướng dẫn công tác huấn luyện ATLĐ, VSLĐ</a></li>
+						<li class="linkItem"><a>Thông tư 41/2011/TT-BLĐTBXH sửa đổi bổ sung một số quy định của Thông tư 37/2005/TT-BLĐTBXH</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+        <div class="featureContent featureEven">
+			<h2 class="title">Kiểm Tra - Thử Nghiệm</h2>
+			<div class="textFeature">
+				<div class="a">
+					<div class="imageWrapper">
+						<img class="image" src="http://kiemdinh.vn/images/h4.gif" alt="" />
+					</div>
+					<span>Trung tâm đi tiên phong trong việc cung cấp các dịch vụ kiểm tra không phá hủy hiện đại
+                    và tin cậy nhất cho kết cấu thép và mối hàn: siêu âm, chụp ảnh bức xạ, bột từ, thẩm thấu,
+                    kiểm tra truyền âm (acoustic emmision), siêu âm đa biến tử (phased array ultrasonic test) v.v
+                    đồng thời là đơn vị có uy tín trong lĩnh vực kiểm tra hệ thống chống sét, đánh giá mức độ an toàn
+                    chống điện giật các hệ thống điện hạ áp.</span>
+					<a class="more"><span>Xem chi tiết</span></a>
+				</div>
+				<div class="linksWrapper">
+					<ul class="moreLinks">
+						<li class="linkItem"><a>Gửi yêu cầu kiểm tra, thử nghiệm</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+        <div class="featureContent featureOdd">
+			<h2 class="title">Tư Vấn Giám Sát</h2>
+			<div class="textFeature">
+				<div class="a">
+					<div class="imageWrapper">
+						<img class="image" src="http://kiemdinh.vn/images/h5.gif" alt="" />
+					</div>
+					<span>Không chỉ thực hiện công việc kiểm định nhà nước theo luật định,
+                    Trung tâm còn là đơn vị có uy tín tại Việt Nam trong lĩnh vực thẩm định thiết kế,
+                    giám sát chế tạo, lắp đặt các bình áp lực, nồi hơi, thiết bị nâng, các đường ống
+                    dẫn hơi, dẫn khí đốt.</span>
+					<a class="more"><span>Xem chi tiết</span></a>
+				</div>
+				<div class="linksWrapper">
+					<ul class="moreLinks">
+						<li class="linkItem"><a>Gửi yêu cầu tư vấn giám sát</a></li>
 					</ul>
 				</div>
 			</div>
